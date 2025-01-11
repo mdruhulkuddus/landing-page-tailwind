cb9b3a031f1475db1bb84bf29fab1dcbe4600217
ruhulkuddus-dsi <ruhul.kuddus@dsinnovators.com>
2025-01-11 12:53:15 +0600

    vite and new deploy setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./**/*.{html,js}",
 "!./node_modules/**/*"   // Exclude the node_modules directory
   ],
